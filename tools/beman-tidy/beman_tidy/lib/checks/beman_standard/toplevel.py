bd918de06df8e98f67608dc5650803ce8ae8c9cf
wermos <63574588+wermos@users.noreply.github.com>
2025-06-24 12:39:04 +0530

    beman-tidy: implement TOPLEVEL.LICENSE and TOPLEVEL:README
    Simple checks to ensure that the top-level LICENSE and README files
    exist and are not empty.

--- tools/beman-tidy/beman_tidy/lib/checks/beman_standard/toplevel.py
+++ tools/beman-tidy/beman_tidy/lib/checks/beman_standard/toplevel.py
@@ -2,6 +2,8 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 from .cmake import CMakeBaseCheck
+from ..base.file_base_check import FileBaseCheck
+from ..base.readme_base_check import ReadmeBaseCheck
 from ..system.registry import register_beman_standard_check
 
 # [TOPLEVEL.*] checks category.
@@ -22,7 +24,57 @@ class ToplevelCmakeCheck(CMakeBaseCheck):
         # TODO: Implement the fix.
         pass
 
+@register_beman_standard_check("TOPLEVEL.LICENSE")
+class ToplevelLicenseCheck(FileBaseCheck):
+    def __init__(self, repo_info, beman_standard_check_config):
+        super().__init__(repo_info, beman_standard_check_config)
 
-# TODO TOPLEVEL.LICENSE - use FileBaseCheck
+    def check(self):
+        self.path = self.repo_path / self.config["file_name"]
 
-# TODO TOPLEVEL.README - use ReadmeBaseCheck
+        if not self.path.exists():
+            self.log("The LICENSE file does not exist.")
+            return False
+
+        try:
+            with open(self.path, 'r') as file:
+                if len(file.read()) == 0:
+                    self.log("The LICENSE file is empty.")
+                    return False
+        except Exception:
+            self.log("Failed to read the LICENSE file.")
+            return False
+
+        return True
+    
+    def fix(self):
+        # TODO: Implement the fix.
+        pass
+
+@register_beman_standard_check("TOPLEVEL.README")
+class ToplevelLicenseCheck(ReadmeBaseCheck):
+    def __init__(self, repo_info, beman_standard_check_config):
+        super().__init__(repo_info, beman_standard_check_config)
+
+    def check(self):
+        self.path = self.repo_path / self.config["file_name"]
+
+        if not self.path.exists():
+            self.log("The README file does not exist.")
+            return False
+
+        try:
+            with open(self.path, 'r') as file:
+                if len(file.read()) == 0:
+                    self.log("The README file is empty.")
+                    return False
+        except Exception:
+            self.log("Failed to read the README file.")
+            return False
+
+        return True
+    
+    def fix(self):
+        # TODO: Implement the fix.
+        pass
+
