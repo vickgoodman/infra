ba0d6444bb6f440319fea399e2b31eef43ab85a3
wermos <63574588+wermos@users.noreply.github.com>
2025-06-24 12:43:56 +0530

    Fix import path.

--- tools/beman-tidy/beman_tidy/lib/checks/beman_standard/toplevel.py
+++ tools/beman-tidy/beman_tidy/lib/checks/beman_standard/toplevel.py
@@ -3,7 +3,7 @@
 
 from .cmake import CMakeBaseCheck
 from ..base.file_base_check import FileBaseCheck
-from ..base.readme_base_check import ReadmeBaseCheck
+from .readme import ReadmeBaseCheck
 from ..system.registry import register_beman_standard_check
 
 # [TOPLEVEL.*] checks category.
