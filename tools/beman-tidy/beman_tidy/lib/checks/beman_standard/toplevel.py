5c4ae0c5d4c837f8b91d8b4a51640608f71b1a83
rishyak <hello@rishyak.com>
2025-06-28 20:02:31 -0400

    refactor: using existing logic to check top-level CML
    - previously, we were hand-rolling the checks for the existence of the file, and whether or not the file is empty
    - FileBaseCheck already provides these tests

--- tools/beman-tidy/beman_tidy/lib/checks/beman_standard/toplevel.py
+++ tools/beman-tidy/beman_tidy/lib/checks/beman_standard/toplevel.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-from ..base.base_check import BaseCheck
+from .cmake import CMakeBaseCheck
 from ..system.registry import register_beman_standard_check
 
 # [TOPLEVEL.*] checks category.
@@ -10,42 +10,19 @@ from ..system.registry import register_beman_standard_check
 # Note: ToplevelBaseCheck is not a registered check!
 
 
-class ToplevelBaseCheck(BaseCheck):
-    def __init__(self, repo_info, beman_standard_check_config):
-        super().__init__(repo_info, beman_standard_check_config)
-
-@register_beman_standard_check("TOPLEVEL.CMAKE")
-class ToplevelCmakeCheck(ToplevelBaseCheck):
+@register_beman_standard_check(check="TOPLEVEL.CMAKE")
+class ToplevelCmakeCheck(CMakeBaseCheck):
     def __init__(self, repo_info, beman_standard_check_config):
         super().__init__(repo_info, beman_standard_check_config)
 
     def check(self):
-        """
-        self.config["value"] contains the CMake file name.
-        """
-        self.path = self.repo_path / self.config["value"]
-        assert self.config["value"].endswith(".txt")
-
-        if not self.path.exists():
-            self.log("The top-level CMake file does not exist.")
-            return False
-
-        try:
-            with open(self.path, 'r') as file:
-                if len(file.read()) == 0:
-                    self.log("The top-level CMake file is empty.")
-                    return False
-        except Exception:
-            self.log("Failed to read the top-level CMake file.")
-            return False
-
-        return True
+        return super().pre_check()
 
     def fix(self):
         # TODO: Implement the fix.
         pass
 
-# TODO TOPLEVEL.LICENSE - use FileBaseCheck
 
+# TODO TOPLEVEL.LICENSE - use FileBaseCheck
 
 # TODO TOPLEVEL.README - use ReadmeBaseCheck
