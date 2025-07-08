78de7eb4ff54bd12c9abb790107edc86bcda07d8
Eddie Nolan <eddiejnolan@gmail.com>
2025-07-07 22:58:17 -0400

    Update cmake/llvm-libc++-toolchain.cmake
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- cmake/llvm-libc++-toolchain.cmake
+++ cmake/llvm-libc++-toolchain.cmake
@@ -16,5 +16,5 @@
 include(${CMAKE_CURRENT_LIST_DIR}/llvm-toolchain.cmake)
 
 if(NOT CMAKE_CXX_FLAGS MATCHES "-stdlib=libc\\+\\+")
-  string(APPEND CMAKE_CXX_FLAGS " -stdlib=libc++")
+    string(APPEND CMAKE_CXX_FLAGS " -stdlib=libc++")
 endif()
