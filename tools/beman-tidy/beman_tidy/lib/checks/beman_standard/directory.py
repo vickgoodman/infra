cfa3fd36506d91070c04a8f3d0c4d2d341737951
Darius Neațu <neautdarius@gmail.com>
2025-07-13 11:22:18 +0300

    [beman-tidy] Fix broken tests after making DIRECTORY.SOURCES to be conditional

--- tools/beman-tidy/beman_tidy/lib/checks/beman_standard/directory.py
+++ tools/beman-tidy/beman_tidy/lib/checks/beman_standard/directory.py
@@ -39,6 +39,7 @@ class BemanTreeDirectoryCheck(DirectoryBaseCheck):
 class DirectorySourcesCheck(BemanTreeDirectoryCheck):
     """
     Check if the sources directory is src/beman/<short_name>.
+    Note: Allow header-only libraries (missing any source files location).
 
     Example for a repo named "exemplar": src/beman/exemplar
     """
@@ -46,30 +47,39 @@ class DirectorySourcesCheck(BemanTreeDirectoryCheck):
     def __init__(self, repo_info, beman_standard_check_config):
         super().__init__(repo_info, beman_standard_check_config, "src")
 
+    def pre_check(self):
+        # Need to override this, because DIRECTORY.SOURCES is conditional
+        # (a repo without any source files location is still valid - header only libraries)
+        return True
+
     def check(self):
-        # Check if path with prefix `src/` exits.
-        src_path = self.repo_path / "src/"
-        if src_path.exists():
-            # Check self.path (src/beman/$library) exists and is not empty.
-            return (
-                self.pre_check()
-            )  
-        # Should not allow known source locations.
-        for prefix in ["source", "sources", "lib", "library"]:
-            prefix_path = self.repo_path / prefix
-            if prefix_path.exists():
+        # Should not allow other known source locations.
+        forbidden_source_locations = ["source/", "sources/", "lib/", "library/"]
+        for forbidden_prefix in forbidden_source_locations:
+            forbidden_prefix = self.repo_path / forbidden_prefix
+            if forbidden_prefix.exists():
                 self.log(
-                    f"Please move sources from {prefix} to src/beman/{self.repo_name}. See https://github.com/bemanproject/beman/blob/main/docs/BEMAN_STANDARD.md#directorysources for more information."
+                    f"Please move source files from {forbidden_prefix} to src/beman/{self.repo_name}. See https://github.com/bemanproject/beman/blob/main/docs/BEMAN_STANDARD.md#directorysources for more information."
                 )
                 return False
 
-        # Probably it's a header only library, we validate the current structure.
+        # If `src/` exists, src/beman/<short_name> also should exist.
+        if (self.repo_path / "src/").exists() and not self.path.exists():
+            self.log(
+                f"Please use the required source files location: src/beman/{self.repo_name}. See https://github.com/bemanproject/beman/blob/main/docs/BEMAN_STANDARD.md#directorysources for more information."
+            )
+            return False
+
+        # Valid source file location or missing -> Beman Standard compliant.
         return True
 
     def fix(self):
-        self.log(
-            f"Please move sources to src/beman/{self.repo_name}. See https://github.com/bemanproject/beman/blob/main/docs/BEMAN_STANDARD.md#directorysources for more information."
-        )
+        # Because we don't know which is the actually invalid source file locations,
+        # we cannot do a proper implementation for fix().
+        if not self.check():
+            self.log(
+                f"Please manually move sources to src/beman/{self.repo_name}. See https://github.com/bemanproject/beman/blob/main/docs/BEMAN_STANDARD.md#directorysources for more information."
+            )
 
 
 # TODO DIRECTORY.TESTS
