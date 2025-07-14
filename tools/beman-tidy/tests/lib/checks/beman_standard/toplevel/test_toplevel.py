8c56c92c208f9d2ce14870498a2253415dd122cb
Sava Victor <victor.sava.00@gmail.com>
2025-07-14 16:46:40 +0300

    [beman-tidy] Clean up comments and repaired wrong path names

--- tools/beman-tidy/tests/lib/checks/beman_standard/toplevel/test_toplevel.py
+++ tools/beman-tidy/tests/lib/checks/beman_standard/toplevel/test_toplevel.py
@@ -24,6 +24,7 @@ def test__TOPLEVEL_CMAKE__valid(repo_info, beman_standard_check_config):
     Test that repositories with valid CMakeLists.txt pass the check.
     """
     valid_cmake_paths = [
+        # exemplar/ repo with non-empty CMakeLists.txt file.
         Path(f"{valid_prefix}/repo-exemplar-v1/"),
     ]
 
@@ -67,13 +68,14 @@ def test__TOPLEVEL_LICENSE__valid(repo_info, beman_standard_check_config):
     """
     Test that repositories with valid LICENSE pass the check.
     """
-    valid_cmake_paths = [
+    valid_license_paths = [
+        # exemplar/ repo with non-empty LICENSE file.
         Path(f"{valid_prefix}/repo-exemplar-v1/"),
     ]
 
     run_check_for_each_path(
         True,
-        valid_cmake_paths,
+        valid_license_paths,
         ToplevelLicenseCheck,
         repo_info,
         beman_standard_check_config,
@@ -84,7 +86,7 @@ def test__TOPLEVEL_LICENSE__invalid(repo_info, beman_standard_check_config):
     """
     Test that repositories with invalid LICENSE fail the check.
     """
-    invalid_cmake_paths = [
+    invalid_license_paths = [
         # exemplar/ repo with empty LICENSE file.
         Path(f"{invalid_prefix}/repo-exemplar-v1/"),
         # exemplar/ repo with LICENSE in non-root location.
@@ -95,7 +97,7 @@ def test__TOPLEVEL_LICENSE__invalid(repo_info, beman_standard_check_config):
 
     run_check_for_each_path(
         False,
-        invalid_cmake_paths,
+        invalid_license_paths,
         ToplevelLicenseCheck,
         repo_info,
         beman_standard_check_config,
