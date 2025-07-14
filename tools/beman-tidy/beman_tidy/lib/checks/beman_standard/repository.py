f8d426e901d80fd14c58ad7b9ae9dae6789043ff
Sava Victor <victor.sava.00@gmail.com>
2025-07-14 18:35:06 +0300

    [beman-tidy] Implement check() for repository default branch

--- tools/beman-tidy/beman_tidy/lib/checks/beman_standard/repository.py
+++ tools/beman-tidy/beman_tidy/lib/checks/beman_standard/repository.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 from ..base.file_base_check import FileBaseCheck
+from ..base.base_check import BaseCheck
 from ..system.registry import register_beman_standard_check
 
 # [REPOSITORY.*] checks category.
@@ -26,6 +27,26 @@ class RepositoryCodeownersCheck(FileBaseCheck):
     def fix(self):
         self.log(
             "Please add a CODEOWNERS file to the repository. See https://github.com/bemanproject/beman/blob/main/docs/BEMAN_STANDARD.md#repositorycodeowners for more information."
+        )
+
+
+@register_beman_standard_check("REPOSITORY.DEFAULT_BRANCH")
+class RepositoryDefaultBranchCheck(BaseCheck):
+    def __init__(self, repo_info, beman_standard_check_config):
+        super().__init__(repo_info, beman_standard_check_config)
+
+    def check(self):
+        if self.repo_info["default_branch"] == "main":
+            return True
+
+        self.log(
+            f"The default branch of the repository should be 'main', but it is '{self.repo_info['default_branch']}'."
+        )
+        return False
+
+    def fix(self):
+        self.log(
+            "Please change the default branch to 'main'. This typically needs to be done through your Git hosting platform (GitHub, GitLab, etc.)."
         )
 
 
