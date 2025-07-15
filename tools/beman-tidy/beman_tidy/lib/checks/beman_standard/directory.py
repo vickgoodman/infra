819668344c6d493f96f51dfd902a323d28964316
Sava Victor <victor.sava.00@gmail.com>
2025-07-15 16:29:28 +0300

    [beman-tidy] Implement DIRECTORY.INTERFACE_HEADERS check and add tests

--- tools/beman-tidy/beman_tidy/lib/checks/beman_standard/directory.py
+++ tools/beman-tidy/beman_tidy/lib/checks/beman_standard/directory.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+import os
 from ..base.directory_base_check import DirectoryBaseCheck
 from ..system.registry import register_beman_standard_check
 
@@ -19,7 +20,51 @@ class BemanTreeDirectoryCheck(DirectoryBaseCheck):
         )
 
 
-# TODO DIRECTORY.INTERFACE_HEADERS
+@register_beman_standard_check("DIRECTORY.INTERFACE_HEADERS")
+class DirectoryInterfaceHeadersCheck(DirectoryBaseCheck):
+    def __init__(self, repo_info, beman_standard_check_config):
+        super().__init__(repo_info, beman_standard_check_config, ".")
+
+    def check(self):
+        """
+        Check that all public header files reside within the include/beman/<short_name>/ directory.
+        """
+        # Check if the path exists.
+        # Example path: "exemplar/include/beman/exemplar"
+        include_path = self.path / "include" / "beman" / self.repo_info["name"]
+        if not os.path.exists(include_path) or os.path.isfile(include_path) or len(os.listdir(include_path)) == 0:
+            self.log(
+                f"The path '{self.path}' does not exist, is a file or is empty."
+                " All public header files must reside within include/beman/<short_name>/."
+            )
+            return False
+
+        # Get all .hpp files paths, excluding certain directories.
+        exclude_dirs = {'src'}
+        if self.repo_info["name"] == "exemplar":
+            exclude_dirs.add('cookiecutter')
+
+        hpp_files = []
+        for root, dirs, files in os.walk(self.path):
+            dirs[:] = [d for d in dirs if d not in exclude_dirs]
+
+            for name in files:
+                if name.lower().endswith('.hpp'):
+                    hpp_files.append(os.path.join(root, name))
+
+        # Check that all .hpp files are under the include/beman/<short_name>/
+        for hpp_file in hpp_files:
+            if not hpp_file.startswith(str(include_path)):
+                self.log(
+                    f"Header file '{hpp_file}' is not under include/beman/{self.repo_info['name']}/ directory."
+                    " All public header files must reside within include/beman/<short_name>/."
+                )
+                return False
+
+        return True
+
+    def fix(self):
+        pass
 
 
 # TODO DIRECTORY.IMPLEMENTATION_HEADERS
---
+++ tools/beman-tidy/tests/lib/checks/beman_standard/directory/test_directory.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+import pytest
+from pathlib import Path
+
+from tests.utils.path_runners import (
+    run_check_for_each_path,
+)
+
+# Actual tested checks.
+from beman_tidy.lib.checks.beman_standard.directory import (
+    DirectoryInterfaceHeadersCheck,
+)
+
+test_data_prefix = "tests/lib/checks/beman_standard/directory/data"
+valid_prefix = f"{test_data_prefix}/valid"
+invalid_prefix = f"{test_data_prefix}/invalid"
+
+
+def test__DIRECTORY_INTERFACE_HEADERS__valid(repo_info, beman_standard_check_config):
+    """
+    Test that repositories with valid public header files pass the check.
+    """
+    valid_headers_paths = [
+        # exemplar/ repo with valid public headers.
+        Path(f"{valid_prefix}/repo-exemplar-v1/"),
+    ]
+
+    run_check_for_each_path(
+        True,
+        valid_headers_paths,
+        DirectoryInterfaceHeadersCheck,
+        repo_info,
+        beman_standard_check_config,
+    )
+
+def test__DIRECTORY_INTERFACE_HEADERS__invalid(repo_info, beman_standard_check_config):
+    """
+    Test that repositories with invalid public header files fail the check.
+    """
+    invalid_headers_paths = [
+        # exemplar/ repo without include/beman/<short_name>/ directory.
+        Path(f"{invalid_prefix}/repo-exemplar-v1/"),
+        # exemplar/ repo with include/beman/<short_name>/ directory but no public headers.
+        Path(f"{invalid_prefix}/repo-exemplar-v2/"),
+        # exemplar/ repo with public headers outside include/beman/<short_name>/ directory.
+        Path(f"{invalid_prefix}/repo-exemplar-v3/"),
+    ]
+
+    run_check_for_each_path(
+        False,
+        invalid_headers_paths,
+        DirectoryInterfaceHeadersCheck,
+        repo_info,
+        beman_standard_check_config,
+    )
+
+
+@pytest.mark.skip(reason="NOT implemented")
+def test__DIRECTORY_INTERFACE_HEADERS__fix_inplace(repo_info, beman_standard_check_config):
+    pass
