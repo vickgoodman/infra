aa24c86bf30c22ef101a82abf407f22f3ce1eec7
Sava Victor <victor.sava.00@gmail.com>
2025-07-16 16:01:05 +0300

    [beman-tidy] Added check and tests for LICENSE.APPROVED

--- tools/beman-tidy/beman_tidy/lib/checks/beman_standard/license.py
+++ tools/beman-tidy/beman_tidy/lib/checks/beman_standard/license.py
@@ -2,6 +2,8 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 from ..base.file_base_check import FileBaseCheck
+from ..system.registry import register_beman_standard_check
+
 
 # [LICENSE.*] checks category.
 # All checks in this file extend the LicenseBaseCheck class.
@@ -14,7 +16,35 @@ class LicenseBaseCheck(FileBaseCheck):
         super().__init__(repo_info, beman_standard_check_config, "LICENSE")
 
 
-# TODO LICENSE.APPROVED
+@register_beman_standard_check("LICENSE.APPROVED")
+class LicenseApprovedCheck(LicenseBaseCheck):
+    def __init__(self, repo_info, beman_standard_check_config):
+        super().__init__(repo_info, beman_standard_check_config)
+
+    def check(self):
+        lines = self.read_lines_strip()
+
+        approved_licenses = [
+            "Apache License",
+            "Boost Software License",
+            "MIT License"
+        ]
+
+        for line in lines:
+            if any(license_name in line for license_name in approved_licenses):
+                return True
+
+        self.log(
+            f"The file '{self.path}' does not contain an approved license. "
+            "See https://github.com/bemanproject/beman/blob/main/docs/BEMAN_STANDARD.md#licenseapproved for more information."
+        )
+        return False
+
+    def fix(self):
+        self.log(
+            "Please update the LICENSE file to include an approved license. "
+            "See https://github.com/bemanproject/beman/blob/main/docs/BEMAN_STANDARD.md#licenseapproved for more information."
+        )
 
 
 # TODO LICENSE.APACHE_LLVM
