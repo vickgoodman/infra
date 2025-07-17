a84f179f9783a0ebf0bf069201bf10378f27b1ff
Darius Neațu <neautdarius@gmail.com>
2025-07-17 16:03:13 +0300

    tweaks

--- tools/beman-tidy/beman_tidy/lib/checks/beman_standard/license.py
+++ tools/beman-tidy/beman_tidy/lib/checks/beman_standard/license.py
@@ -3,6 +3,8 @@
 
 from ..base.file_base_check import FileBaseCheck
 from ..system.registry import register_beman_standard_check
+import re
+import textwrap
 
 
 # [LICENSE.*] checks category.
@@ -22,63 +24,37 @@ class LicenseApprovedCheck(LicenseBaseCheck):
         super().__init__(repo_info, beman_standard_check_config)
 
     def check(self):
-        lines = self.read_lines_strip()
+        license_text = self.read()
 
-        separator_line = "=============================================================================="
-        approved_licenses = [
-            "Apache License v2.0 with LLVM Exceptions",
-            "Boost Software License 1.0",
-            "MIT License",
-        ]
+        pattern = re.compile(
+            textwrap.dedent(r"""
+            ^={78}\n
+            The Beman Project is under the
+                (Apache License v2\.0 with LLVM Exceptions|
+                Boost Software License 1\.0|
+                MIT License):
+            \n={78}\n
 
-        # Check if license header is correct
-        license_header = lines[0:3]
+            (.+?)  # Non-empty license body
 
-        has_approved_license_header = False
-        for approved_license in approved_licenses:
-            if (
-                license_header[0] == separator_line
-                and license_header[1]
-                == f"The Beman Project is under the {approved_license}:"
-                and license_header[2] == separator_line
-            ):
-                has_approved_license_header = True
+            ={78}\n
+            Software from third parties included in the Beman Project:\n
+            ={78}\n
+            The Beman Project contains third party software which is under different license
+            terms\. All such code will be identified clearly using at least one of two
+            mechanisms:\n
+            1\) It will be in a separate directory tree with its own `LICENSE\.txt` or
+            `LICENSE` file at the top containing the specific license and restrictions
+            which apply to that software, or\n
+            2\) It will contain specific license and restriction terms at the top of every
+            file\.
+        """),
+            re.DOTALL | re.VERBOSE,
+        )
 
-        if not has_approved_license_header:
+        if not pattern.match(license_text):
             self.log(
-                "Incorrect header for LICENSE. "
-                "See https://github.com/bemanproject/beman/blob/main/docs/BEMAN_STANDARD.md#licenseapproved for more information."
-            )
-            return False
-
-        # Check if license footer is correct
-        license_footer = lines[-11:]
-
-        correct_license_footer = [
-            "==============================================================================",
-            "Software from third parties included in the Beman Project:",
-            "==============================================================================",
-            "The Beman Project contains third party software which is under different license",
-            "terms. All such code will be identified clearly using at least one of two",
-            "mechanisms:",
-            "1) It will be in a separate directory tree with its own `LICENSE.txt` or",
-            "`LICENSE` file at the top containing the specific license and restrictions",
-            "which apply to that software, or",
-            "2) It will contain specific license and restriction terms at the top of every",
-            "file.",
-        ]
-
-        if license_footer != correct_license_footer:
-            self.log(
-                "Incorrect footer for LICENSE. "
-                "See https://github.com/bemanproject/beman/blob/main/docs/BEMAN_STANDARD.md#licenseapproved for more information."
-            )
-            return False
-
-        # Check if there's any content between the header and footer
-        if len(lines) < 17:
-            self.log(
-                "LICENSE contains only footer and header. There should be actual license content. "
+                "The LICENSE file does not contain an approved license. "
                 "See https://github.com/bemanproject/beman/blob/main/docs/BEMAN_STANDARD.md#licenseapproved for more information."
             )
             return False
