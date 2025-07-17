2bd3185e2ee59c84bf48931bcc3f2838a225b01b
Sava Victor <victor.sava.00@gmail.com>
2025-07-17 14:29:02 +0300

    [beman-tidy] Run linter

--- tools/beman-tidy/beman_tidy/lib/checks/beman_standard/license.py
+++ tools/beman-tidy/beman_tidy/lib/checks/beman_standard/license.py
@@ -25,16 +25,23 @@ class LicenseApprovedCheck(LicenseBaseCheck):
         lines = self.read_lines_strip()
 
         separator_line = "=============================================================================="
-        approved_licenses = ["Apache License v2.0 with LLVM Exceptions", "Boost Software License 1.0", "MIT License"]
+        approved_licenses = [
+            "Apache License v2.0 with LLVM Exceptions",
+            "Boost Software License 1.0",
+            "MIT License",
+        ]
 
         # Check if license header is correct
         license_header = lines[0:3]
 
         has_approved_license_header = False
         for approved_license in approved_licenses:
-            if (license_header[0] == separator_line
-            and license_header[1] == f"The Beman Project is under the {approved_license}:"
-            and license_header[2] == separator_line):
+            if (
+                license_header[0] == separator_line
+                and license_header[1]
+                == f"The Beman Project is under the {approved_license}:"
+                and license_header[2] == separator_line
+            ):
                 has_approved_license_header = True
 
         if has_approved_license_header == False:
@@ -58,21 +65,21 @@ class LicenseApprovedCheck(LicenseBaseCheck):
             "`LICENSE` file at the top containing the specific license and restrictions",
             "which apply to that software, or",
             "2) It will contain specific license and restriction terms at the top of every",
-            "file."
+            "file.",
         ]
 
         if license_footer != correct_license_footer:
             self.log(
-            "Incorrect footer for LICENSE. "
-            "See https://github.com/bemanproject/beman/blob/main/docs/BEMAN_STANDARD.md#licenseapproved for more information."
+                "Incorrect footer for LICENSE. "
+                "See https://github.com/bemanproject/beman/blob/main/docs/BEMAN_STANDARD.md#licenseapproved for more information."
             )
             return False
 
         # Check if there's any content between the header and footer
         if len(lines) < 17:
             self.log(
-            "LICENSE contains only footer and header. There should be actual license content. "
-            "See https://github.com/bemanproject/beman/blob/main/docs/BEMAN_STANDARD.md#licenseapproved for more information."
+                "LICENSE contains only footer and header. There should be actual license content. "
+                "See https://github.com/bemanproject/beman/blob/main/docs/BEMAN_STANDARD.md#licenseapproved for more information."
             )
             return False
 
