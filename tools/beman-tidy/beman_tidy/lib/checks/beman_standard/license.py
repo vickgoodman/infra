15d92ebf006813cd77eafeb3f3cbba643d6c98ac
Sava Victor <victor.sava.00@gmail.com>
2025-07-18 11:16:28 +0300

    [beman-tidy] Removed dead code

--- tools/beman-tidy/beman_tidy/lib/checks/beman_standard/license.py
+++ tools/beman-tidy/beman_tidy/lib/checks/beman_standard/license.py
@@ -27,28 +27,6 @@ class LicenseApprovedCheck(LicenseBaseCheck):
         content = self.read()
 
         # Regex pattern for LICENSE check
-        """
-        pattern = re.compile(
-            r'^={78}\n'
-            r'The Beman Project is under the (?:Apache License v2\.0 with LLVM Exceptions|Boost Software License 1\.0|MIT License):\n'
-            r'={78}\n'
-            r'.+?\n'  # Any content (non-greedy)
-            r'={78}\n'
-            r'Software from third parties included in the Beman Project:\n'
-            r'={78}\n'
-            r'The Beman Project contains third party software which is under different license\n'
-            r'terms\. All such code will be identified clearly using at least one of two\n'
-            r'mechanisms:\n'
-            r'1\) It will be in a separate directory tree with its own `LICENSE\.txt` or\n'
-            r'   `LICENSE` file at the top containing the specific license and restrictions\n'
-            r'   which apply to that software, or\n'
-            r'2\) It will contain specific license and restriction terms at the top of every\n'
-            r'   file\.$',
-            re.DOTALL
-        )
-        # """
-
-        # """
         pattern = re.compile(
             r"""^={78}
 The Beman Project is under the (Apache License v2\.0 with LLVM Exceptions|Boost Software License 1\.0|MIT License):
@@ -68,7 +46,6 @@ mechanisms:
 """,
             re.DOTALL,
         )
-        # """
 
         if not pattern.match(content):
             self.log(
