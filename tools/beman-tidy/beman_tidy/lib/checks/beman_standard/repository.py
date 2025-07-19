211fccc8a6940f60b52bf4e79bb19d4c173b964c
Sava Victor <victor.sava.00@gmail.com>
2025-07-19 16:07:17 +0300

    [beman-tidy] Added check, fix and tests for REPOSITORY.NAME

--- tools/beman-tidy/beman_tidy/lib/checks/beman_standard/repository.py
+++ tools/beman-tidy/beman_tidy/lib/checks/beman_standard/repository.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+import re
+
 from ..base.file_base_check import FileBaseCheck
 from ..base.base_check import BaseCheck
 from ..system.registry import register_beman_standard_check
@@ -11,7 +13,43 @@ from ..system.registry import register_beman_standard_check
 # Note: FileBaseCheck is not a registered check!
 
 
-# TODO REPOSITORY.NAME
+@register_beman_standard_check("REPOSITORY.NAME")
+class RepositoryNameCheck(BaseCheck):
+    def __init__(self, repo_info, beman_standard_check_config):
+        super().__init__(repo_info, beman_standard_check_config)
+
+    def check(self):
+        def is_snake_case(name):
+            return re.match("(^[a-z0-9]+$)|(^[a-z0-9][a-z0-9_.]+[a-z0-9]$)", name)
+
+        def is_beman_snake_case(name):
+            """
+            Has prefix "beman." and continues with snake_case.
+            It must NOT end with a C++ target standard version - e.g. 17, 20, 23, 26, 32, etc.
+            """
+            return (
+                name[:6] != "beman."
+                and is_snake_case(name)
+                and not re.match(".*[0-9]+$", name)
+            )
+
+        name = self.repo_info["name"]
+
+        if not is_beman_snake_case(name):
+            self.log(
+                "The repository should be named after the library name excluding the 'beman.' prefix. It should not contain a target C++ version. "
+                "See https://github.com/bemanproject/beman/blob/main/docs/BEMAN_STANDARD.md#repositoryname for more information."
+            )
+            return False
+
+        return True
+
+    def fix(self):
+        self.log(
+            "beman-tidy can't automatically fix the repository name since that would require GitHub API calls and administrative permissions. "
+            "Please see https://github.com/bemanproject/beman/blob/main/docs/BEMAN_STANDARD.md#repositoryname for more information."
+        )
+        pass
 
 
 @register_beman_standard_check("REPOSITORY.CODEOWNERS")
