7fe0d692959e831f681280ddce0a9bfec06c4325
Sava Victor <victor.sava.00@gmail.com>
2025-07-20 13:51:35 +0300

    [beman-tidy] Added check, fix and tests for DIRECTORY.DOCS

--- tools/beman-tidy/beman_tidy/lib/checks/beman_standard/directory.py
+++ tools/beman-tidy/beman_tidy/lib/checks/beman_standard/directory.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+from pathlib import Path
+
 from ..base.directory_base_check import DirectoryBaseCheck
 from ..system.registry import register_beman_standard_check
 
@@ -90,7 +92,61 @@ class DirectorySourcesCheck(BemanTreeDirectoryCheck):
 # TODO DIRECTORY.EXAMPLES
 
 
-# TODO DIRECTORY.DOCS
+@register_beman_standard_check("DIRECTORY.DOCS")
+class DirectoryDocsCheck(DirectoryBaseCheck):
+    """
+    Check if the all documentation files reside within docs/ directory.
+    Exception: root README.md file.
+    """
+
+    def __init__(self, repo_info, beman_standard_check_config):
+        super().__init__(repo_info, beman_standard_check_config, "docs")
+
+    def pre_check(self):
+        # Need to override this, because DIRECTORY.DOCS is conditional
+        # (a repo without any documentation is still valid)
+        return True
+
+    def check(self):
+        repo_path = Path(self.repo_path)
+        root_readme = repo_path / 'README.md'
+
+        # Exclude directories that are not part of the documentation.
+        exclude_dirs = ['papers', '.github']
+
+        if self.path.exists():
+            exclude_dirs.append('docs')
+
+        if self.repo_name == 'exemplar':
+            exclude_dirs.extend(['cookiecutter', 'infra'])
+
+        # Find all markdown files in the repository.
+        misplaced_md_files = [
+            p for p in repo_path.rglob('*.md')
+            if not any(excluded in p.parts for excluded in exclude_dirs)  # exclude files in excluded directories
+            and p != root_readme  # exclude root README.md
+        ]
+
+        # Check if any markdown files are misplaced.
+        if len(misplaced_md_files) > 0:
+            for misplaced_md_file in misplaced_md_files:
+                self.log(f"Misplaced markdown file found: {misplaced_md_file}")
+
+            self.log(
+                "Please move all documentation files within the docs/ directory, except for the root README.md file. "
+                "See https://github.com/bemanproject/beman/blob/main/docs/BEMAN_STANDARD.md#directorydocs for more information."
+            )
+
+            return False
+
+        # Check passes if there is no docs/ directory or no misplaced markdown files are found
+        return True
+
+    def fix(self):
+        self.log(
+            "Please manually move documentation files to the docs/ directory, except for the root README.md file. "
+            "See https://github.com/bemanproject/beman/blob/main/docs/BEMAN_STANDARD.md#directorydocs for more information."
+        )
 
 
 # TODO DIRECTORY.PAPERS
