36482bc68b6e6261cfe26efd551377c61ecdd4a4
Sava Victor <victor.sava.00@gmail.com>
2025-08-11 20:58:17 +0300

    [beman-tidy] Run linter

--- tools/beman-tidy/beman_tidy/lib/checks/beman_standard/directory.py
+++ tools/beman-tidy/beman_tidy/lib/checks/beman_standard/directory.py
@@ -207,11 +207,11 @@ class DirectoryPapersCheck(DirectoryBaseCheck):
             └── abstract.bst
         """
         # Exclude directories that are not part of the papers/ directory.
-        exclude_dirs = ["src", "docs", "examples"]
+        exclude_dirs = ["src", "docs", "examples", ".github"]
         if self.path.exists():
             exclude_dirs.append("papers")
         if self.repo_name == "exemplar":
-            exclude_dirs.append("cookiecutter")
+            exclude_dirs.extend(["cookiecutter", "infra"])
 
         # File extensions that are considered "paper-related"
         paper_extensions = [
@@ -243,7 +243,10 @@ class DirectoryPapersCheck(DirectoryBaseCheck):
         for extension in paper_extensions:
             for p in self.repo_path.rglob(f"*{extension}"):
                 # Exclude files that are already in excluded directories.
-                if not any(excluded in str(p) for excluded in exclude_dirs) and p != self.repo_path / "README.md":
+                if (
+                    not any(excluded in str(p) for excluded in exclude_dirs)
+                    and p != self.repo_path / "README.md"
+                ):
                     misplaced_paper_files.append(p)
 
         if len(misplaced_paper_files) > 0:
